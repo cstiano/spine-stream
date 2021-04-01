3f1df0aaf4602d9539d601e0e76cd2f94d76323f
Cristiano Oliveira <cso@cin.ufpe.br>
2021-04-01 09:12:37 -0300

    test(Coroutine Playground): includes coroutine playground unit test

---
+++ spine-stream-lib/src/test/java/com/spinestream/core/CoroutinePlaygroundUnitTest.kt
@@ -0,0 +1,68 @@
+package com.spinestream.core
+
+import kotlinx.coroutines.*
+import org.junit.Test
+
+class CoroutinePlaygroundUnitTest {
+
+    @Test
+    fun launch() {
+        /*
+        launch coroutine creates a background routine without blocking
+        the thread that launched it. inherited the context of where it
+        was called if is not defined.
+         */
+
+        GlobalScope.launch {
+            println("Running launch coroutine")
+        }
+    }
+
+    @Test
+    fun async() {
+        /*
+        with async you can do a barrier to return the results until the
+        end of the execution.
+         */
+        runBlocking {
+            val asyncResult = fetchResult().await()
+            println(asyncResult)
+        }
+    }
+
+    @Test
+    fun withContext() {
+        /*
+        withContext coroutine let you set a specific context to associate
+        the coroutine.
+         */
+
+        runBlocking {
+            // Associate with main thread
+//            GlobalScope.launch(context = Dispatchers.Main) {
+//                println("Main")
+//            }
+
+            // Associate with IO operations thread
+            GlobalScope.launch(context = Dispatchers.IO) {
+                println("IO")
+            }
+
+            // Associate with background thread
+            GlobalScope.launch(context = Dispatchers.Default) {
+                println("Background")
+            }
+
+            // When don't need to execute in a specific thread
+            GlobalScope.launch(context = Dispatchers.Unconfined) {
+                println("Unconfined")
+            }
+        }
+    }
+
+    // auxiliary function
+    suspend fun fetchResult() = GlobalScope.async {
+        delay(2000)
+        "The end"
+    }
+}
