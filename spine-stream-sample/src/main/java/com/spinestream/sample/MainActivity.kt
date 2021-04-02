67cce304d5c97f1bf6fa17f2b23f8a5533cb3168
Cristiano Oliveira <cso@cin.ufpe.br>
2021-04-02 10:34:49 -0300

    feat(content provider): adds vanilla content provider inicialization

--- spine-stream-sample/src/main/java/com/spinestream/sample/MainActivity.kt
+++ spine-stream-sample/src/main/java/com/spinestream/sample/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.spinestream.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.spinestream.core.ToasterMessage
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        ToasterMessage.s(this, "test message")
     }
 }
---
+++ spine-stream-sdk/src/main/java/com/spinestream/core/InitProvider.kt
@@ -0,0 +1,34 @@
+package com.spinestream.core
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+
+abstract class InitProvider : ContentProvider() {
+
+    override final fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override final fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? = null
+
+    override final fun getType(uri: Uri): String? = null
+
+    override final fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int = 0
+
+    override final fun delete(
+        uri: Uri,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int = 0
+}
